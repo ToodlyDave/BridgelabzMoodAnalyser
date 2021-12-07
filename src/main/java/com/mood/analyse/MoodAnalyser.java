b28fbe747ae7b28b026fe8fba34c6800f98d272c
ToodlyDave <alapatdavid@gmail.com>
2021-12-07 10:25:34 +0530

    REFACTOR code to inform user if null or empty string

--- src/main/java/com/mood/analyse/MoodAnalyser.java
+++ src/main/java/com/mood/analyse/MoodAnalyser.java
@@ -1,5 +1,7 @@
 package com.mood.analyse;
 
+import com.mood.exception.MoodAnalysisException;
+
 public class MoodAnalyser {
 
 	private String message;
@@ -13,15 +15,15 @@ public class MoodAnalyser {
 		this.message = message;
 	}
 
-	public String moodAnalyse() throws NullPointerException{
-		try {
-			String words[] = message.split(" ");
-			if (words[3].equalsIgnoreCase("sad"))
-				return "SAD";
-			return "HAPPY";
-		} catch (NullPointerException e) {
-			// TODO Auto-generated catch block
-			return "HAPPY";
-		}
+	public String moodAnalyse() throws Exception {
+		if (message == null)
+			throw new MoodAnalysisException(" The string is null");
+		if (message.equals(""))
+			throw new MoodAnalysisException(" The string is empty");
+
+		String words[] = message.split(" ");
+		if (words[3].equalsIgnoreCase("sad"))
+			return "SAD";
+		return "HAPPY";
 	}
 }
---
+++ src/main/java/com/mood/exception/MoodAnalysisException.java
@@ -0,0 +1,21 @@
+package com.mood.exception;
+
+public class MoodAnalysisException extends Exception{
+
+	public MoodAnalysisException() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public MoodAnalysisException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+	public MoodAnalysisException(String message, Throwable e) {
+		// TODO Auto-generated constructor stub
+		super(message, e);
+	}
+	
+	
+}
